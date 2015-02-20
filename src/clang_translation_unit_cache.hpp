c0e8f93be7032eb3c12801d2e508f1dd5548bd7d
Robin <me@invokr.org>
2015-02-20 14:11:32 +0000

    Implemented tu cache serialize / unserialize

--- src/clang_translation_unit_cache.hpp
+++ src/clang_translation_unit_cache.hpp
@@ -27,7 +27,10 @@
 #include <string>
 #include <mutex>
 #include <thread>
+#include <fstream>
+#include <iostream>
 
+#include "sha1.hpp"
 #include "noncopyable.hpp"
 #include "clang_translation_unit.hpp"
 
@@ -76,14 +79,50 @@ namespace clang {
             mContainer.clear();
         }
 
-        /** Serializes cache to path */
-        void serialize(const char* path) {
+        /** Serializes cache to path with a unique id identifying the changes */
+        void serialize(const char* path, const char* hash) {
+            std::string p(path);
 
+            std::ofstream output(std::string(p+"db.idx").c_str(), std::ofstream::out);
+            output << mContainer.size(); // number of .unit files
+            output << hash << std::endl; // sha1 of argument set
+
+            uint32_t idx = 0;
+            for (auto &unit : mContainer) {
+                output << unit.first << std::endl;
+                unsigned error = clang_saveTranslationUnit(unit.second->ptr(), std::string(p+std::to_string(idx++)+".unit").c_str(), 0);
+                if (error != 0) {
+                    std::cout << "Error: " << error << std::endl;
+                }
+            }
+
+            output.close();
         }
-        
-        /** Loads cache from path */
-        void unserialize(const char* path) {
 
+        /** Loads cache from path */
+        void unserialize(const char* path, const char* hash, CXIndex idx) {
+            std::string p(path);
+
+            std::ifstream input(std::string(p+"db.idx").c_str(), std::ifstream::in);
+            size_type size;
+            std::string n_hash;
+
+            input >> size;
+            input >> n_hash;
+
+            if (n_hash.compare(hash) != 0) {
+                input.close();
+                return; // compiler arguments have changed, all tu's are invalid
+            }
+
+            std::string key;
+            for (size_type i = 0; i < size; ++i) {
+                input >> key;
+                mContainer[key] = std::make_shared<translation_unit>(clang_createTranslationUnit(idx, std::string(p+std::to_string(i)+".unit").c_str()));
+                mContainer[key]->reparse();
+            }
+
+            input.close();
         }
     private:
         container_t mContainer;
